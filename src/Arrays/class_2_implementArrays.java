cc40f3958a8f468605a6b44e1e97317a843df90f
ranjan <ranjankumar4278907@gmail.com>
2025-02-01 12:15:31 +0530

    Lesson_1

---
+++ src/Arrays/class_2_implementArrays.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class class_2_implementArrays {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 9;
+        arr[1] = 8;
+        arr[2] = 7;
+        arr[3] = 4;
+
+        // Print the Array
+
+        for (int i = 0; i < 4; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
