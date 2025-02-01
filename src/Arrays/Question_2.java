9f3c28f44a056180bb91f8a799ad735b9ea2112d
ranjan <ranjankumar4278907@gmail.com>
2025-02-01 21:46:34 +0530

    Lesson_1

---
+++ src/Arrays/Question_2.java
@@ -0,0 +1,30 @@
+// Search the Given elements X in the array if Present then return the index else return -1;
+
+// input : [1,5,6,7,4] , X = 6
+// output : 2
+
+package Arrays;
+
+public class Question_2 {
+    static int SearchElement(int[] arr,int X){
+
+        int n = arr.length;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == X){
+                return i;
+
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {1,5,6,7,4};
+        int X = 0;
+
+        int result = SearchElement(arr,X);
+        System.out.println(result);
+
+    }
+}
