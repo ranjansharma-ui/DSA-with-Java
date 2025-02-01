a1304d39b4e18da7d1b3eed406c0e7ca2e512a3d
ranjan <ranjankumar4278907@gmail.com>
2025-02-01 21:37:20 +0530

    Lesson_1

---
+++ src/Arrays/Question_1.java
@@ -0,0 +1,27 @@
+// calculate the maximum value out of all the elements in the array.
+//intput : [1,2,3,4,2,4]
+// output : 4
+
+
+
+package Arrays;
+
+public class Question_1 {
+    public static int Max(int arr[]){
+        int ans = 0;
+
+        for (int i=0; i<arr.length; i++){
+            if(arr[i] > ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,2,4};
+        int result = Max(arr);
+        System.out.println(result);
+
+    }
+}
