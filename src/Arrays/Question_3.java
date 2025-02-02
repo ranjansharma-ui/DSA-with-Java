e9a5fbc521edc70c5d1a9b73c64bd439847e6641
ranjan <ranjankumar4278907@gmail.com>
2025-02-02 15:15:22 +0530

    Question

---
+++ src/Arrays/Question_3.java
@@ -0,0 +1,23 @@
+// count the number of occurrence of a particular element x
+
+package Arrays;
+
+public class Question_3 {
+    public static int countoccurr(int arr[],int x){
+        int count = 0;
+        for (int i=0; i<arr.length; i++){
+            if(arr[i] == x){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] ={5,6,1,5};
+        int ans = countoccurr(arr,5);
+        System.out.println(ans);
+
+    }
+
+}
