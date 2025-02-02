abb35dad73ab714137b96723135bf465eb3a6cc2
ranjan <ranjankumar4278907@gmail.com>
2025-02-02 15:08:41 +0530

    Lesson_1

---
+++ src/Arrays/Arrays_Cloning.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Arrays_Cloning {
+    static void PrintingArray(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,4,3,6};
+        int arr_2[] = arr;  // same address
+
+        int arr_3[] = arr.clone();  // New Address , Original array never change when are changed copy array
+
+        arr_3[2] = 0;
+        PrintingArray(arr_3);
+        System.out.println();
+        PrintingArray(arr);
+
+
+
+        // similar methode like 1> Arrays.copyof(arr,arr.length)
+        //                       2> Arrays.copyOfRange(arr,1,4)
+
+
+
+
+
+
+    }
+}
