fb9269f890bc0edbc37f22978ea8c39e7e0f5e43
ranjan <ranjankumar4278907@gmail.com>
2025-05-07 14:32:36 +0530

    Question

--- src/Arrays/InterviewQuestion_Leetcode_GFG/Kadanes_Algo/MaxSumInSubArr.java
+++ src/Arrays/InterviewQuestion_Leetcode_GFG/Kadanes_Algo/MaxSumInSubArr.java
@@ -1,25 +1,26 @@
 package Arrays.InterviewQuestion_Leetcode_GFG.Kadanes_Algo;
 
 public class MaxSumInSubArr {
-    public static int kandanes(int arr[]){
+    public static int kandanes(int arr[]) {
         int n = arr.length;
 
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
 
-        for (int i=0; i<n; i++){
-            currSum = currSum+arr[i];
+        for (int i = 0; i < n; i++) {
+            currSum = currSum + arr[i];
 
-            if (currSum < 0){
+            if (currSum < 0) {
                 currSum = 0;
             }
 
-            maxSum = Math.max(currSum,maxSum);
+            maxSum = Math.max(currSum, maxSum);
         }
         return maxSum;
     }
+
     public static void main(String[] args) {
-        int arr[] = {-2,-3,4,-1,-2,1,5,-3};
-        System.out.println("Max Subarray sum is : "+kandanes(arr));
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println("Max Subarray sum is : " + kandanes(arr));
     }
 }
